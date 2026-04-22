83e76ed0de611c7e1136d4637bcad905c50c23a5
markysteinberg <markj62959@gmail.com>
2026-04-21 23:56:51 -0400

    idiot

--- includes/shader.h
+++ includes/shader.h
@@ -22,7 +22,7 @@ public:
 
     void setMat4(const std::string& name, const glm::mat4& mat) const;
     void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, int value) const;
+    void setFloat(const std::string& name, float value) const;
 
 private: 
     void check_compile(GLuint shader);
